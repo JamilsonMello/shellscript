030394b5ac99cc130c043411392c8c08648cbc8d
jamilsonmello <jamilsonmelow@gmail.com>
2021-02-11 20:51:33 -0300

    create variables

---
+++ variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+name="Jamilson"
+last_name="Jamilson"
+
+echo "meu nome é :" $name
+
+echo "meu sobrenome é :" $last_name
+
+#variable to execute some command
+
+date_command=$(date)
+sistem_info=`df -h`
+
+echo "$date_command"
+
+echo "$sistem_info"
+
