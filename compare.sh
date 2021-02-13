5c599f5170b5584c981178640acc0440a7795302
jamilsonmello <jamilsonmelow@gmail.com>
2021-02-12 23:35:05 -0300

    commands to campare values

---
+++ compare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+-eq Equal
+-ne Different
+-gt Bigger
+-lt Less
+-o  or
+-d  is a directory
+-e  if exists
+-z  if it was not filled
+-f  has text
+-o  if the user is owner
+-r  can read the file?
+-w  can write in the file?
+-x  can execute the file?
